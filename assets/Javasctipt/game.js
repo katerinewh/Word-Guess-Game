6cdae6f52030f1a197e32ce4c9e59d8bc4eff7e2
Katerine <katerinehw@att.net>
2018-11-08 10:08:45 -0800

    Thursday morning

---
+++ assets/Javasctipt/game.js
@@ -0,0 +1,8 @@
+ var prompt("Welcome to my word game.  What's your name?"); 
+    console.log ("Hello " + name);
+confirm("Would you ike to play a game with me, " + name +"?")
+if (name) {
+    confirm("Awesome, let's play!");
+}
+    else (name){ 
+       confirm("That's okay, maybe another day.");
